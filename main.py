8dbb1a7b1d6f4ad160162ea2c7aea37b999335a4
Patrick Bray <patrickbray17@gmail.com>
2026-05-28 16:49:08 -0400

    add dotenv

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 from models import StoryCard
 from mock_data import MOCK_CARDS
 
+load_dotenv()
+
 app = FastAPI(title="Prediction News API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=os.getenv("CORS_ORIGINS", "http://localhost:5173").split(","),
     allow_methods=["GET"],
     allow_headers=["*"],
 )
